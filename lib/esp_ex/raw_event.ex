31f5183598ac36dc2c82111c777a2cd56648acaf
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2018-05-10 14:18:05 -0700

    Add RawEvent basic struct

--- lib/esp_ex/raw_event.ex
+++ lib/esp_ex/raw_event.ex
@@ -1,5 +1,21 @@
 defmodule EspEx.RawEvent do
+  alias EspEx.RawEvent.Metadata
+
   @type t :: struct
 
-  defstruct []
+  @enforce_keys [
+    :id,
+    :stream_name,
+    :type,
+    :data,
+    :metadata
+  ]
+  defstruct id: "",
+            stream_name: "",
+            type: "",
+            position: nil,
+            global_position: nil,
+            data: %{},
+            metadata: %Metadata{},
+            time: nil
 end

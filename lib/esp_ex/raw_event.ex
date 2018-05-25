c4c0ae5f400dd6e71cf209f2f2a57febfd886335
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2018-05-25 00:17:12 -0700

    Fix next_position to return no_stream

--- lib/esp_ex/raw_event.ex
+++ lib/esp_ex/raw_event.ex
@@ -40,10 +40,10 @@ defmodule EspEx.RawEvent do
     Map.put(other_event, :metadata, other_meta)
   end
 
-  def next_position(nil), do: -1
+  def next_position(nil), do: :no_stream
   def next_position(position) when is_integer(position), do: position + 1
 
-  def next_global_position(nil), do: -1
+  def next_global_position(nil), do: :no_stream
 
   def next_global_position(global_position) when is_integer(global_position) do
     global_position + 1

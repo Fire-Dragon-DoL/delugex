2633d7f5a020b79f71e49cbe9ab759a0069378d2
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2018-11-07 18:21:36 -0800

    Remove Application module

--- mix.exs
+++ mix.exs
@@ -26,8 +26,7 @@ defmodule Delugex.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      mod: {Delugex.Application, []}
+      extra_applications: [:logger]
     ]
   end
 
